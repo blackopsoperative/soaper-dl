68a07a8ae04c75ed061f9c7737833eb2419d1cbf
blackopsoperative <116139554+blackopsoperative@users.noreply.github.com>
2023-08-16 14:40:35 -0400

    Extend soaper-dl's functionality.
    This adds;
    The ability to set a specific directory with -x. (does not have a default due to not knowing how to do that, will fix later.)
    Fixes an error with movies/shows that has it not download due to having a colon in it's name.
    Moved all temporary files to /tmp/soaper-dl (folder still needs to be created every time the script is ran, will fix later.)
    Fixes a spelling mistake in select_episodes_to_download function.
    And probably much more that I've forgotten.

--- soaper-dl.sh
+++ soaper-dl.sh
@@ -34,9 +34,12 @@ set_var() {
 
     _HOST="https://soaper.tv"
     _SEARCH_URL="$_HOST/search/keyword/"
-
-    _SCRIPT_PATH=$(dirname "$(realpath "$0")")
-    _SEARCH_LIST_FILE="${_SCRIPT_PATH}/search.list"
+    
+    _INSTALL_DIR=$(dirname "$(realpath "$0")")
+    _SCRIPT_PATH=${x:-$_INSTALL_DIR}
+    _TMP_DIR="/tmp/soaper-dl"
+    
+    _SEARCH_LIST_FILE="search.list"
     _SOURCE_FILE=".source.html"
     _EPISODE_LINK_LIST=".episode.link"
     _EPISODE_TITLE_LIST=".episode.title"
@@ -73,6 +76,9 @@ set_args() {
             \?)
                 print_error "Invalid option: -$OPTARG"
                 ;;
+            x)
+                _SCRIPT_PATH="$OPTARG" && echo $_SCRIPT_PATH | sed 's/.$//'
+                ;;
         esac
     done
 }
@@ -104,7 +110,7 @@ sed_remove_space() {
 
 download_media_html() {
     # $1: media link
-    "$_CURL" -sS "${_HOST}${1}" > "$_SCRIPT_PATH/$_MEDIA_NAME/$_MEDIA_HTML"
+    "$_CURL" -sS "${_HOST}${1}" > "$_TMP_DIR/$_MEDIA_HTML"
 }
 
 get_media_name() {
@@ -123,12 +129,12 @@ search_media_by_name() {
     len="$(grep -c "class=\"thumbnail" <<< "$t")"
     [[ -z "$len" || "$len" == "0" ]] && print_error "Media not found!"
 
-    true > "$_SEARCH_LIST_FILE"
+    true > "$_TMP_DIR/$_SEARCH_LIST_FILE"
     for i in $(seq 1 "$len"); do
         n="$($_PUP ".thumbnail:nth-child($i) h5 a:nth-child(1) text{}" <<< "$t" | sed_remove_space)"
         l="$($_PUP ".thumbnail:nth-child($i) h5 a:nth-child(1) attr{href}" <<< "$t" | sed_remove_space)"
         lb="$($_PUP --charset UTF-8 ".thumbnail:nth-child($i) .label-info text{}" <<< "$t" | sed_remove_space)"
-        echo "[$l][$lb] $n" | tee -a "$_SEARCH_LIST_FILE"
+        echo "[$l][$lb] $n" | tee -a "$_TMP_DIR/$_SEARCH_LIST_FILE"
     done
 }
 
@@ -145,7 +151,7 @@ download_source() {
     if is_movie "$_MEDIA_PATH"; then
         download_media "$_MEDIA_PATH" "$_MEDIA_NAME"
     else
-        echo "$a" > "$_SCRIPT_PATH/$_MEDIA_NAME/$_SOURCE_FILE"
+        echo "$a" > "$_TMP_DIR/$_SOURCE_FILE"
     fi
 }
 
@@ -188,7 +194,8 @@ download_episodes() {
 download_episode() {
     # $1: episode number
     local l
-    l=$(grep "\[$1\] " "$_SCRIPT_PATH/$_MEDIA_NAME/$_EPISODE_LINK_LIST" \
+    #l=$(grep "\[$1\] " "$_SCRIPT_PATH/$_MEDIA_NAME/$_EPISODE_LINK_LIST" \ ORIGINAL
+    l=$(grep "\[$1\] " "$_TMP_DIR/$_EPISODE_LINK_LIST" \
         | awk -F '] ' '{print $2}')
     [[ "$l" != *"/"* ]] && print_error "Wrong download link or episode not found!"
     download_media "$l" "$1"
@@ -235,9 +242,9 @@ download_media() {
 
 create_episode_list() {
     local slen sf t l sn et el
-    sf="$_SCRIPT_PATH/$_MEDIA_NAME/$_SOURCE_FILE"
-    el="$_SCRIPT_PATH/$_MEDIA_NAME/$_EPISODE_LINK_LIST"
-    et="$_SCRIPT_PATH/$_MEDIA_NAME/$_EPISODE_TITLE_LIST"
+    sf="$_TMP_DIR/$_SOURCE_FILE"
+    el="$_TMP_DIR/$_EPISODE_LINK_LIST"
+    et="$_TMP_DIR/$_EPISODE_TITLE_LIST"
     slen="$(grep 'alert alert-info-ex' -c "$sf")"
     true > "$et"
     true > "$el"
@@ -258,27 +265,28 @@ create_episode_list() {
 
 select_episodes_to_download() {
     cat "$_SCRIPT_PATH/$_MEDIA_NAME/$_EPISODE_TITLE_LIST" >&2
-    echo -n "Which episode(s) to downolad: " >&2
+    echo -n "Which episode(s) to download: " >&2
     read -r s
     echo "$s"
 }
 
 main() {
-    set_args "$@"
-    set_var
+    set_var && set_args "$@"
 
     local mlist=""
     if [[ -n "${_INPUT_NAME:-}" ]]; then
         mlist="$(search_media_by_name "$_INPUT_NAME")"
         _MEDIA_PATH=$($_FZF -1 <<< "$(sort -u <<< "$mlist")" | awk -F']' '{print $1}' | sed -E 's/^\[//')
+        echo $_MEDIA_PATH
     fi
 
     [[ -z "${_MEDIA_PATH:-}" ]] && print_error "Media not found! Missing option -n <name> or -p <path>?"
-    [[ ! -s "$_SEARCH_LIST_FILE" ]] && print_error "$_SEARCH_LIST_FILE not found. Please run \`-n <name>\` to generate it."
-    _MEDIA_NAME=$(sort -u "$_SEARCH_LIST_FILE" \
+    [[ ! -s "$_TMP_DIR/$_SEARCH_LIST_FILE" ]] && print_error "$_SEARCH_LIST_FILE not found. Please run \`-n <name>\` to generate it."
+    _MEDIA_NAME=$(sort -u "$_TMP_DIR/$_SEARCH_LIST_FILE" \
                 | grep "$_MEDIA_PATH" \
                 | awk -F '] ' '{print $2}' \
-                | sed -E 's/\//_/g')
+                | sed -E 's/\//_/g' \
+                | tr -d ':')
 
     [[ "$_MEDIA_NAME" == "" ]] && _MEDIA_NAME="$(get_media_name "$_MEDIA_PATH")"
 
